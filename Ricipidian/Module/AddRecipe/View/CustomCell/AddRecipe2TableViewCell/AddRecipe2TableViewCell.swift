9e9b6e2f312d359b866588d679c73be345149ddc
Minh Tâm Nguyễn <minhtamkv@gmail.com>
2021-03-29 21:39:13 +0700

    no message

---
+++ Ricipidian/Module/AddRecipe/View/CustomCell/AddRecipe2TableViewCell/AddRecipe2TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  AddRecipe2TableViewCell.swift
+//  Ricipidian
+//
+//  Created by Minh Tâm on 29/03/2021.
+//
+
+import UIKit
+
+class AddRecipe2TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Ricipidian/Module/AddRecipe/View/CustomCell/MaterialTableViewCell/MaterialTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MaterialTableViewCell.swift
+//  Ricipidian
+//
+//  Created by Minh Tâm on 29/03/2021.
+//
+
+import UIKit
+
+class MaterialTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
